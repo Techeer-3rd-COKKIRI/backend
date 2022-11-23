6ef0448f82ebcb3fdd4cd13e9efb4788faaffc16
JeongHunHui <deca3248@gmail.com>
2022-11-23 18:47:15 +0900

    chore: SpringBoot 초기세팅 완료 및 git ignore 수정

---
+++ src/main/java/com/techeer/cokkiri/CokkiriApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.cokkiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokkiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokkiriApplication.class, args);
+	}
+
+}
