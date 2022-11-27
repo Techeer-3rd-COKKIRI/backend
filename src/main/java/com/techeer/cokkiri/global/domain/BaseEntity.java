dc60f147ddd6f7ba3ee11232f5c560f7d582b3aa
JeongHunHui <deca3248@gmail.com>
2022-11-27 18:58:43 +0900

    feat: 모든 엔티티의 부모클래스인 BaseEntity 클래스 생성

---
+++ src/main/java/com/techeer/cokkiri/global/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.techeer.cokkiri.global.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "CREATED_AT")
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+
+    @Getter
+    @Column(name = "IS_DELETED", nullable = false)
+    protected boolean isDeleted;
+
+    public void deleteEntity() {
+        this.isDeleted = true;
+    }
+}
