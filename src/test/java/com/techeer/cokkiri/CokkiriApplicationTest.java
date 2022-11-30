0124a384bcec39a9ec7b359ba053bcb0600992b7
JeongHunHui <deca3248@gmail.com>
2022-11-30 14:15:07 +0900

    chore: 테스트용 DB H2 세팅

---
+++ src/test/java/com/techeer/cokkiri/CokkiriApplicationTest.java
@@ -0,0 +1,10 @@
+package com.techeer.cokkiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CokkiriApplicationTest {
+    @Test
+    void contextLoads() {}
+}
