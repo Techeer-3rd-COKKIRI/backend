1b07949f36198c86d6c981143a015d8243a99716
JeongHunHui <deca3248@gmail.com>
2022-12-02 03:46:16 +0900

    fix: BaseEntity의 기본값을 주는 방식 변경 및 기타 사항 변경

--- src/main/java/com/techeer/cokkiri/global/entity/BaseEntity.java
+++ src/main/java/com/techeer/cokkiri/global/entity/BaseEntity.java
@@ -4,21 +4,26 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import lombok.Getter;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@ToString
 public class BaseEntity {
   @CreatedDate private LocalDateTime createdAt;
 
   @LastModifiedDate private LocalDateTime updatedAt;
 
   @Column(columnDefinition = "boolean default false")
-  private boolean isDeleted;
+  private Boolean isDeleted;
 
   public void deleteEntity() {
     this.isDeleted = true;
