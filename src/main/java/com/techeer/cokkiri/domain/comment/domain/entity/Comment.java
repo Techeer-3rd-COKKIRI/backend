651a7d9b071a79b5870a292763e5f6afc504fce4
kimich <ickim1218@gmail.com>
2022-12-01 20:50:16 +0900

    fix: 불필요한 column 삭제 및 타입 수정

--- src/main/java/com/techeer/cokkiri/domain/comment/domain/entity/Comment.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/domain/entity/Comment.java
@@ -3,23 +3,24 @@ package com.techeer.cokkiri.domain.comment.domain.entity;
 import com.techeer.cokkiri.global.domain.BaseEntity;
 import javax.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Getter
 @Entity
-@AllArgsConstructor
+@DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "COMMENT")
+@Table
 public class Comment extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "COMMENT_ID")
   private Long id;
 
-  @Column(name = "USER_ID", nullable = false)
+  @Column(nullable = false)
   private Long userId;
 
-  @Column(name = "STUDY_ID", nullable = false)
+  @Column(nullable = false)
   private Long studyId;
 
   @Column(name = "COMMENT_ADDRESS", length = 255, nullable = false)
@@ -29,10 +30,12 @@ public class Comment extends BaseEntity {
   private Integer studyWeek;
 
   @Column(name = "LIKE")
-  private Boolean likeCount;
+  @ColumnDefault("0")
+  private Integer likeCount;
 
   @Column(name = "DISLIKE")
-  private Boolean dislikeCount;
+  @ColumnDefault("0")
+  private Integer dislikeCount;
 
   @Column(name = "IS_NOTICED")
   private Boolean isNoticed;
@@ -51,15 +54,15 @@ public class Comment extends BaseEntity {
       Long studyId,
       String commentAddress,
       Integer studyWeek,
-      Boolean likeCount,
-      Boolean dislikeCount) {
+      Integer likeCount,
+      Integer dislikeCount,
+      Boolean isNoticed) {
     this.userId = userId;
     this.studyId = studyId;
     this.commentAddress = commentAddress;
     this.studyWeek = studyWeek;
     this.likeCount = likeCount;
     this.dislikeCount = dislikeCount;
-
-    super.isDeleted = false;
+    this.isNoticed = isNoticed;
   }
 }
