bd0e51d5c4f8bb87d40572873a9ed56d6150ea69
kimich <ickim1218@gmail.com>
2022-12-01 15:27:36 +0900

    feat: Comment 엔티티 구현

---
+++ src/main/java/com/techeer/cokkiri/domain/example/domain/entity/Comment.java
@@ -0,0 +1,62 @@
+package com.techeer.cokkiri.domain.example.domain.entity;
+
+import com.techeer.cokkiri.global.domain.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "COMMENT")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+    private Long id;
+
+    @Column(name = "USER_ID", nullable = false)
+    private Long userId;
+
+    @Column(name = "STUDY_ID", nullable = false)
+    private Long studyId;
+
+    @Column(name = "COMMENT_ADDRESS", length = 255, nullable = false)
+    private String commentAddress;
+
+    @Column(name = "STUDY_WEEK")
+    private Integer studyWeek;
+
+    @Column(name = "LIKE")
+    private Boolean likeCount;
+
+    @Column(name = "DISLIKE")
+    private Boolean dislikeCount;
+
+    @Column(name = "IS_NOTICED")
+    private Boolean isNoticed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STUDY_ID", nullable = false)
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+
+    @Builder
+    public Comment(Long userId, Long studyId, String commentAddress, Integer studyWeek, Boolean likeCount, Boolean dislikeCount) {
+        this.userId = userId;
+        this.studyId = studyId;
+        this.commentAddress = commentAddress;
+        this.studyWeek = studyWeek;
+        this.likeCount = likeCount;
+        this.dislikeCount = dislikeCount;
+
+        super.isDeleted = false;
+    }
+
+}
