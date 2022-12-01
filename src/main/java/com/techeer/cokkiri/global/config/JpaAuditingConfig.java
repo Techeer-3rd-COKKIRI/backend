8652651841a7a83fff07d31c3d8f7c670ccc632e
JeongHunHui <deca3248@gmail.com>
2022-12-01 22:29:09 +0900

    feat: JpaAuditingConfig 파일 추가(BaseEntity의 CreatedDate와 LastModifiedDate 정상 작동)

---
+++ src/main/java/com/techeer/cokkiri/global/config/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.techeer.cokkiri.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {}
