4efd06351adba6ca27a3f0b4d183c8623d1274cc
kimich <ickim1218@gmail.com>
2022-12-04 15:43:52 +0900

    fix: 기본 값 설정 및 불필요한 어노테이션 삭제

--- src/main/java/com/techeer/cokkiri/domain/comment/domain/entity/Comment.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/domain/entity/Comment.java
@@ -1,68 +1,67 @@
 package com.techeer.cokkiri.domain.comment.domain.entity;
 
-import com.techeer.cokkiri.global.domain.BaseEntity;
 import javax.persistence.*;
+
+import com.techeer.cokkiri.global.entity.BaseEntity;
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 @Getter
 @Entity
 @DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table
+@ToString(callSuper = true)
 public class Comment extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "COMMENT_ID")
+    private Long id;
 
-  @Column(nullable = false)
-  private Long userId;
+    @Column(nullable = false)
+    private Long userId;
 
-  @Column(nullable = false)
-  private Long studyId;
+    @Column(nullable = false)
+    private Long studyId;
 
-  @Column(name = "COMMENT_ADDRESS", length = 255, nullable = false)
-  private String commentAddress;
+    @Column(name = "COMMENT_ADDRESS", length = 255, nullable = false)
+    private String commentAddress;
 
-  @Column(name = "STUDY_WEEK")
-  private Integer studyWeek;
+    @Column(name = "STUDY_WEEK")
+    private Integer studyWeek;
 
-  @Column(name = "LIKE")
-  @ColumnDefault("0")
-  private Integer likeCount;
+    @Column(name = "LIKE", columnDefinition = "0")
+    private Integer likeCount;
 
-  @Column(name = "DISLIKE")
-  @ColumnDefault("0")
-  private Integer dislikeCount;
+    @Column(name = "DISLIKE", columnDefinition = "0")
+    private Integer dislikeCount;
 
-  @Column(name = "IS_NOTICED")
-  private Boolean isNoticed;
+    @Column(name = "IS_NOTICED")
+    private Boolean isNoticed;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+ /* @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "STUDY_ID", nullable = false)
   private Study study;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "USER_ID", nullable = false)
-  private User user;
+  private User user;*/
 
-  @Builder
-  public Comment(
-      Long userId,
-      Long studyId,
-      String commentAddress,
-      Integer studyWeek,
-      Integer likeCount,
-      Integer dislikeCount,
-      Boolean isNoticed) {
-    this.userId = userId;
-    this.studyId = studyId;
-    this.commentAddress = commentAddress;
-    this.studyWeek = studyWeek;
-    this.likeCount = likeCount;
-    this.dislikeCount = dislikeCount;
-    this.isNoticed = isNoticed;
-  }
+    @Builder
+    private Comment(
+            Long userId,
+            Long studyId,
+            String commentAddress,
+            Integer studyWeek,
+            Integer likeCount,
+            Integer dislikeCount,
+            Boolean isNoticed) {
+        this.userId = userId;
+        this.studyId = studyId;
+        this.commentAddress = commentAddress;
+        this.studyWeek = studyWeek;
+        this.likeCount = likeCount;
+        this.dislikeCount = dislikeCount;
+        this.isNoticed = isNoticed;
+    }
 }
