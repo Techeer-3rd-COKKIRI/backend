0521ee2932972251e58af5827f075611c02d65e0
Heekyeong Park <qkrgmlrud00@gmail.com>
2022-12-08 18:53:12 +0900

    feat: UserRepository 생성

--- src/main/java/com/techeer/cokkiri/domain/user/repository/UserRepository.java
+++ src/main/java/com/techeer/cokkiri/domain/user/repository/UserRepository.java
@@ -3,4 +3,10 @@ package com.techeer.cokkiri.domain.user.repository;
 import com.techeer.cokkiri.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+
+    Optional<User> findUserByUsername(String username);
+}
