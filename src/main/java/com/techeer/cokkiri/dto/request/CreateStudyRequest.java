ffe5d9432a6a7c2c58fe5a8b6b849858b8519cdf
jhkim <wellsy001012@gmail.com>
2022-12-08 17:15:28 +0900

    feat: study Service, Mapper, Controller

---
+++ src/main/java/com/techeer/cokkiri/dto/request/CreateStudyRequest.java
@@ -0,0 +1,13 @@
+package com.techeer.cokkiri.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateStudyRequest {
+
+
+}
