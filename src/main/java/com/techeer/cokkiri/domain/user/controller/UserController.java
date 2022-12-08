a06191873fa726903a8f4aea3e79addbbfd7b515
JeongHunHui <deca3248@gmail.com>
2022-12-08 18:02:33 +0900

    feat: 테스트용 UserController 생성

---
+++ src/main/java/com/techeer/cokkiri/domain/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.techeer.cokkiri.domain.user.controller;
+
+import com.techeer.cokkiri.domain.user.entity.User;
+import com.techeer.cokkiri.domain.user.service.UserService;
+import com.techeer.cokkiri.global.result.ResultResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.techeer.cokkiri.domain.user.controller.UserController.USER_API_URI;
+import static com.techeer.cokkiri.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping(USER_API_URI)
+public class UserController {
+  public static final String USER_API_URI = "/api/users";
+
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<ResultResponse> registration(@RequestBody User user) {
+    userService.registrationUser(user);
+    return ResponseEntity.ok(ResultResponse.of(USER_REGISTRATION_SUCCESS));
+  }
+}
