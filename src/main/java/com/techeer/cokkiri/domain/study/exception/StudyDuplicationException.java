7a7ffaf0e0e6d4a5625beb77ffe635e7f7c90b09
JeongHunHui <deca3248@gmail.com>
2022-12-12 01:01:06 +0900

    feat: 스터디의 이름이 중복될 때 발생시킬 예외 클래스 추가

---
+++ src/main/java/com/techeer/cokkiri/domain/study/exception/StudyDuplicationException.java
@@ -0,0 +1,10 @@
+package com.techeer.cokkiri.domain.study.exception;
+
+import com.techeer.cokkiri.global.error.ErrorCode;
+import com.techeer.cokkiri.global.error.exception.BusinessException;
+
+public class StudyDuplicationException extends BusinessException {
+    public StudyDuplicationException() {
+        super(ErrorCode.STUDY_DUPLICATION_ERROR);
+    }
+}
