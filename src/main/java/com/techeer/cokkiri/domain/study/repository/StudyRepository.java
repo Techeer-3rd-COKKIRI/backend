bca7af309651544b7aff5f1c888e097cb8ac8361
JeongHunHui <deca3248@gmail.com>
2022-12-12 01:02:06 +0900

    feat: StudyName을 통해 스터디의 존재여부를 확인하는 메소드 추가

--- src/main/java/com/techeer/cokkiri/domain/study/repository/StudyRepository.java
+++ src/main/java/com/techeer/cokkiri/domain/study/repository/StudyRepository.java
@@ -3,4 +3,6 @@ package com.techeer.cokkiri.domain.study.repository;
 import com.techeer.cokkiri.domain.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRepository extends JpaRepository<Study, Long> {}
+public interface StudyRepository extends JpaRepository<Study, Long> {
+    boolean existsByStudyName(String studyName);
+}
