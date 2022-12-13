4be9cf738d19bd829e72d22e7bf4018295cff146
kimich <ickim1218@gmail.com>
2022-12-13 15:04:33 +0900

    fix: 클래스명 변경 및 studyWeek 추가

---
+++ src/main/java/com/techeer/cokkiri/domain/comment/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package com.techeer.cokkiri.domain.comment.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CommentDto {
+
+  private Integer studyWeek;
+  private String content;
+
+}
