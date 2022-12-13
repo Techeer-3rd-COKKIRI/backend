f95af143bb1961b682116e9eb7c20cf820556497
kimich <ickim1218@gmail.com>
2022-12-13 15:03:29 +0900

    feat: Dto를 Entity로 바꿔주는 클래스 생성

---
+++ src/main/java/com/techeer/cokkiri/domain/comment/mapper/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.techeer.cokkiri.domain.comment.mapper;
+
+import com.techeer.cokkiri.domain.comment.dto.CommentDto;
+import com.techeer.cokkiri.domain.comment.entity.Comment;
+
+public class CommentMapper {
+
+    public static Comment toEntity(CommentDto request) {
+
+        return Comment.builder()
+                .content(request.getContent())
+                .studyWeek(request.getStudyWeek())
+                .build();
+    }
+}
