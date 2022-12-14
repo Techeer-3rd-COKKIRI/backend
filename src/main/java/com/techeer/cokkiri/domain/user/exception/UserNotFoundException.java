f28bee2c4aeae474dd4140fc5157998c23405382
Heekyeong Park <qkrgmlrud00@gmail.com>
2022-12-15 00:09:06 +0900

    feat: resultCode, 예외 클래스 추가

--- src/main/java/com/techeer/cokkiri/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/techeer/cokkiri/domain/user/exception/UserNotFoundException.java
@@ -1,11 +1,10 @@
 package com.techeer.cokkiri.domain.user.exception;
 
-import static com.techeer.cokkiri.global.error.ErrorCode.USER_NOT_FOUND_ERROR;
-
+import com.techeer.cokkiri.global.error.ErrorCode;
 import com.techeer.cokkiri.global.error.exception.BusinessException;
 
 public class UserNotFoundException extends BusinessException {
   public UserNotFoundException() {
-    super(USER_NOT_FOUND_ERROR);
+    super(ErrorCode.USER_NOT_FOUND_ERROR);
   }
 }
--- src/main/java/com/techeer/cokkiri/global/result/ResultCode.java
+++ src/main/java/com/techeer/cokkiri/global/result/ResultCode.java
@@ -11,9 +11,13 @@ public enum ResultCode {
   // 도메인 별로 나눠서 관리(ex: User 도메인)
   // user
   USER_REGISTRATION_SUCCESS("U001", "사용자 등록 성공"),
+  USER_USERNAME_DUPLICATED("U002", "회원 아이디 중복"),
+  USER_USERNAME_NOT_DUPLICATED("U003", "회원 아이디 중복되지 않음"),
+  USER_LOGIN_SUCCESS("U004", "회원 로그인 성공"),
+  USER_LOGOUT_SUCCESS("U005", "회원 로그아웃 성공"),
 
   // study
-  STUDY_CREATE_SUCCESS("S001", "스터디 생성 성공"),
+  STUDY_CREATE_SUCCESS("S001", "스터디 생성 성공")
   ;
 
   private final String code;
