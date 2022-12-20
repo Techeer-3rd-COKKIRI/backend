f2a69b1e7c97e3817fbedf3d906222d3ea26fbb0
kimich <ickim1218@gmail.com>
2022-12-20 19:27:26 +0900

    feat: 스터디 주차로 댓글 찾기 구현 및 댓글 없을시 에러 메시지 구현

---
+++ src/main/java/com/techeer/cokkiri/domain/comment/exception/CommentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.techeer.cokkiri.domain.comment.exception;
+
+import com.techeer.cokkiri.global.error.ErrorCode;
+import com.techeer.cokkiri.global.error.exception.BusinessException;
+
+public class CommentNotFoundException extends BusinessException {
+    public CommentNotFoundException() {
+        super(ErrorCode.COMMENT_NOT_FOUND_ERROR);
+    }
+}
--- src/main/java/com/techeer/cokkiri/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,14 @@
 package com.techeer.cokkiri.domain.comment.repository;
 
 import com.techeer.cokkiri.domain.comment.entity.Comment;
+import com.techeer.cokkiri.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findCommentByStudyWeek(Integer studyWeek);
+
+
+}
