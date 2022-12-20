42f80bf21d0e1bdaffbcc1c84e9058cb2b3e1ef2
kimich <ickim1218@gmail.com>
2022-12-20 19:24:54 +0900

    fix: 필요한 파라미터 값 생성

--- src/main/java/com/techeer/cokkiri/domain/comment/service/CommentService.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/service/CommentService.java
@@ -2,8 +2,11 @@ package com.techeer.cokkiri.domain.comment.service;
 
 import com.techeer.cokkiri.domain.comment.dto.CommentDto;
 import com.techeer.cokkiri.domain.comment.entity.Comment;
+import com.techeer.cokkiri.domain.comment.exception.CommentNotFoundException;
 import com.techeer.cokkiri.domain.comment.mapper.CommentMapper;
 import com.techeer.cokkiri.domain.comment.repository.CommentRepository;
+import com.techeer.cokkiri.domain.study.entity.Study;
+import com.techeer.cokkiri.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,12 +15,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentService {
 
-  private final CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
 
-  public void registerComment(CommentDto commentDto) {
+    private final CommentMapper commentMapper;
 
-    Comment comment = CommentMapper.toEntity(commentDto);
 
-    commentRepository.save(comment);
-  }
+    public void registerComment(CommentDto.Request commentDto, User user, Study study) {
+
+        Comment comment = commentMapper.toEntity(commentDto, user, study);
+
+        commentRepository.save(comment);
+    }
+
+    public Comment findCommentByStudyWeek(Integer studyWeek) {
+        return commentRepository.findCommentByStudyWeek(studyWeek).orElseThrow(CommentNotFoundException::new);
+    }
+
+
+
 }
