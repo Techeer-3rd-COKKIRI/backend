976f87c7f33875f0c2a508f2499581408ef8a700
kimich <ickim1218@gmail.com>
2022-12-21 17:27:25 +0900

    fix: 리스트로 변환

--- src/main/java/com/techeer/cokkiri/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.techeer.cokkiri.domain.comment.repository;
 
 import com.techeer.cokkiri.domain.comment.entity.Comment;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-  Optional<Comment> findCommentByStudyWeek(Integer studyWeek);
+  Optional<List<Comment>> findCommentByStudyWeek(Integer studyWeek);
 }
