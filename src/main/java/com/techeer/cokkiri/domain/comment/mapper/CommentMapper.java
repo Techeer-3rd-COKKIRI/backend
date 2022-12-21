0d0b16cf5be68769746a0f8775599a120d7636fb
kimich <ickim1218@gmail.com>
2022-12-21 17:28:36 +0900

    feat: dto에서 댓글정보로 변환해주는 메서드 구현

--- src/main/java/com/techeer/cokkiri/domain/comment/mapper/CommentMapper.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/mapper/CommentMapper.java
@@ -1,10 +1,17 @@
 package com.techeer.cokkiri.domain.comment.mapper;
 
 import com.techeer.cokkiri.domain.comment.dto.CommentDto;
+import com.techeer.cokkiri.domain.comment.dto.CommentInfo;
 import com.techeer.cokkiri.domain.comment.entity.Comment;
+import com.techeer.cokkiri.domain.study.dto.StudyDto;
 import com.techeer.cokkiri.domain.study.entity.Study;
+import com.techeer.cokkiri.domain.user.dto.UserCommentInfo;
 import com.techeer.cokkiri.domain.user.entity.User;
+import com.techeer.cokkiri.global.annotation.LoginUser;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CommentMapper {
@@ -20,5 +27,28 @@ public class CommentMapper {
             .build();
 
     return comment;
+  }
+
+  public CommentInfo toDto(Comment comment) {
+    CommentInfo info =
+            CommentInfo.builder()
+                    .content(comment.getContent())
+                    .likeCount(comment.getLikeCount())
+                    .dislikeCount(comment.getDislikeCount())
+                    .userCommentInfo(CommentMapper.toDto(comment.getUser()))
+                    .build();
+    return info;
+  }
+
+  public static UserCommentInfo toDto(User user) {
+      return UserCommentInfo.builder()
+              .userId(user.getId())
+              .userName(user.getNickname())
+              .imageUrl(user.getImageUrl())
+              .build();
+  }
+
+  public List<CommentInfo> toDtoList(List<Comment> comments) {
+    return comments.stream().map(this::toDto).collect(Collectors.toList());
   }
 }
