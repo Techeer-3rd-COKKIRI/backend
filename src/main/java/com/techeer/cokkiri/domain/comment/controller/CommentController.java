f89b19b77767a6f1b774af369c6b5a7326747b38
kimich <ickim1218@gmail.com>
2022-12-21 17:29:54 +0900

    feat: 스터디주차로 댓글 정보 가져오기 컨트롤러 구현

--- src/main/java/com/techeer/cokkiri/domain/comment/controller/CommentController.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/controller/CommentController.java
@@ -1,6 +1,9 @@
 package com.techeer.cokkiri.domain.comment.controller;
 
 import com.techeer.cokkiri.domain.comment.dto.CommentDto;
+import com.techeer.cokkiri.domain.comment.dto.CommentInfo;
+import com.techeer.cokkiri.domain.comment.entity.Comment;
+import com.techeer.cokkiri.domain.comment.mapper.CommentMapper;
 import com.techeer.cokkiri.domain.comment.service.CommentService;
 import com.techeer.cokkiri.domain.study.entity.Study;
 import com.techeer.cokkiri.domain.study.service.StudyService;
@@ -13,11 +16,10 @@ import javax.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/comments")
 @RestController
@@ -37,5 +39,14 @@ public class CommentController {
     commentService.registerComment(request, user, study);
 
     return ResponseEntity.ok(ResultResponse.of(ResultCode.REGISTER_COMMENT_SUCCESS));
+  }
+
+  @LoginRequired
+  @GetMapping("/{studyWeek}")
+  public ResponseEntity<ResultResponse> findCommentByStudyWeek(@PathVariable Integer studyWeek) {
+
+    List<CommentInfo> comments = commentService.findCommentByStudyWeek(studyWeek);
+
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.COMMENT_FIND_SUCCESS, comments));
   }
 }
