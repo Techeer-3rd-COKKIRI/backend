c1e5f526056d2ed6359eb3bba0dfa63f95b3a47a
kimich <ickim1218@gmail.com>
2022-12-26 16:33:19 +0900

    feat: 스터디아이디를 추가한 commentMapper 추가

--- src/main/java/com/techeer/cokkiri/domain/comment/mapper/CommentMapper.java
+++ src/main/java/com/techeer/cokkiri/domain/comment/mapper/CommentMapper.java
@@ -1,47 +1,54 @@
 package com.techeer.cokkiri.domain.comment.mapper;
 
 import com.techeer.cokkiri.domain.comment.dto.CommentDto;
-import com.techeer.cokkiri.domain.comment.dto.CommentInfo;
 import com.techeer.cokkiri.domain.comment.entity.Comment;
+import com.techeer.cokkiri.domain.study.dto.StudyDto;
 import com.techeer.cokkiri.domain.study.entity.Study;
-import com.techeer.cokkiri.domain.user.dto.UserCommentInfo;
+import com.techeer.cokkiri.domain.user.dto.UserDto;
 import com.techeer.cokkiri.domain.user.entity.User;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentMapper {
 
-  public Comment toEntity(CommentDto.Request dto, User user, Study study) {
+    public Comment toEntity(CommentDto.Request dto, User user, Study study) {
 
-    Comment comment =
-        Comment.builder()
-            .content(dto.getContent())
-            .studyWeek(dto.getStudyWeek())
-            .user(user)
-            .study(study)
-            .build();
+        Comment comment =
+                Comment.builder()
+                        .content(dto.getContent())
+                        .studyWeek(dto.getStudyWeek())
+                        .user(user)
+                        .study(study)
+                        .build();
 
-    return comment;
-  }
+        return comment;
+    }
 
-  public CommentInfo toDto(Comment comment) {
-    CommentInfo info =
-        CommentInfo.builder()
-            .content(comment.getContent())
-            .likeCount(comment.getLikeCount())
-            .dislikeCount(comment.getDislikeCount())
-            .userCommentInfo(CommentMapper.toDto(comment.getUser()))
-            .build();
-    return info;
-  }
+    public CommentDto.ResponseInfo toDto(Comment comment) {
+        CommentDto.ResponseInfo info =
+                CommentDto.ResponseInfo.builder()
+                        .content(comment.getContent())
+                        .likeCount(comment.getLikeCount())
+                        .dislikeCount(comment.getDislikeCount())
+                        .studyCommentInfo(toDto(comment.getStudy()))
+                        .userCommentInfo(toDto(comment.getUser()))
+                        .build();
+        return info;
+    }
 
-  public static UserCommentInfo toDto(User user) {
-    return UserCommentInfo.builder().userId(user.getId()).userName(user.getNickname()).build();
-  }
+    public StudyDto.ResponseCommentInfo toDto(Study study) {
+        return StudyDto.ResponseCommentInfo.builder().studyId(study.getId()).build();
+    }
 
-  public List<CommentInfo> toDtoList(List<Comment> comments) {
-    return comments.stream().map(this::toDto).collect(Collectors.toList());
-  }
+    public UserDto.ResponseCommentInfo toDto(User user) {
+        return UserDto.ResponseCommentInfo.builder().userId(user.getId()).userName(user.getNickname()).build();
+    }
+
+    public List<CommentDto.ResponseInfo> toDtoList(List<Comment> comments) {
+        return comments.stream().map(this::toDto).collect(Collectors.toList());
+    }
 }
