86399dc30921a054520aef7258a400ab50289da0
JeongHunHui <deca3248@gmail.com>
2023-01-02 23:47:44 +0900

    feat: 스터디를 최신순으로 페이징 조회하는 API 구현 완료

--- src/main/java/com/techeer/cokkiri/domain/study/controller/StudyController.java
+++ src/main/java/com/techeer/cokkiri/domain/study/controller/StudyController.java
@@ -1,8 +1,5 @@
 package com.techeer.cokkiri.domain.study.controller;
 
-import static com.techeer.cokkiri.global.result.ResultCode.STUDY_CREATE_SUCCESS;
-import static com.techeer.cokkiri.global.result.ResultCode.STUDY_GET_SUCCESS;
-
 import com.techeer.cokkiri.domain.study.dto.StudyDto;
 import com.techeer.cokkiri.domain.study.exception.StudyDuplicationException;
 import com.techeer.cokkiri.domain.study.service.StudyService;
@@ -14,9 +11,15 @@ import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+import static com.techeer.cokkiri.global.result.ResultCode.*;
 
 @RestController
 @RequestMapping("api/v1/studies")
@@ -40,5 +43,15 @@ public class StudyController {
   public ResponseEntity<ResultResponse> findStudyByStudyId(@PathVariable Long studyId) {
     StudyDto.FindResponse studyResponse = studyService.findStudyDtoById(studyId);
     return ResponseEntity.ok(ResultResponse.of(STUDY_GET_SUCCESS, studyResponse));
+  }
+
+  @ApiOperation(value = "스터디 최신순 페이징 조회")
+  @GetMapping("/page/{page}")
+  public ResponseEntity<ResultResponse> getStudyListWithPagingNewest(
+      @PathVariable Integer page, @RequestParam(defaultValue = "20") Integer size) {
+    PageRequest pageRequest = PageRequest.of(page, size, Sort.by("id").descending());
+    List<StudyDto.InfoResponse> studyInfoResponseList =
+        studyService.getStudyListWithPaging(pageRequest);
+    return ResponseEntity.ok(ResultResponse.of(STUDY_PAGING_GET_SUCCESS, studyInfoResponseList));
   }
 }
