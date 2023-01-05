915787c8aa30985d877bc8bb927aa5570af127a3
JeongHunHui <deca3248@gmail.com>
2023-01-05 23:54:59 +0900

    feat: 로그인되어있는 회원 정보를 조회하는 api 구현

--- src/main/java/com/techeer/cokkiri/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/cokkiri/domain/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.techeer.cokkiri.domain.user.controller;
 
-import static com.techeer.cokkiri.global.result.ResultCode.USER_LOGIN_SUCCESS;
-import static com.techeer.cokkiri.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
-import static com.techeer.cokkiri.global.result.ResultCode.USER_USERNAME_NOT_DUPLICATED;
-
 import com.techeer.cokkiri.domain.user.dto.UserDto;
+import com.techeer.cokkiri.domain.user.entity.User;
 import com.techeer.cokkiri.domain.user.exception.InValidPasswordException;
 import com.techeer.cokkiri.domain.user.exception.UserDuplicatedException;
+import com.techeer.cokkiri.domain.user.mapper.UserMapper;
 import com.techeer.cokkiri.domain.user.service.LoginService;
 import com.techeer.cokkiri.domain.user.service.UserService;
+import com.techeer.cokkiri.global.annotation.LoginRequired;
+import com.techeer.cokkiri.global.annotation.LoginUser;
 import com.techeer.cokkiri.global.result.ResultCode;
 import com.techeer.cokkiri.global.result.ResultResponse;
 import io.swagger.annotations.Api;
@@ -18,6 +18,9 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import static com.techeer.cokkiri.global.result.ResultCode.*;
 
 @Api(tags = "회원 인증 API")
 @RestController
@@ -26,6 +29,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
   private final UserService userService;
   private final LoginService loginService;
+  private final UserMapper userMapper;
 
   @ApiOperation(value = "회원가입")
   @PostMapping
@@ -59,4 +63,13 @@ public class UserController {
     loginService.login(registerResponse.getId());
     return ResponseEntity.ok(ResultResponse.of(USER_LOGIN_SUCCESS, registerResponse));
   }
+
+  @ApiOperation(value = "로그인 사용자 정보 조회")
+  @GetMapping("/login-user")
+  @LoginRequired
+  public ResponseEntity<ResultResponse> getLoginUserInfo(@ApiIgnore @LoginUser User loginUser) {
+    UserDto.RegisterResponse registerResponse = userMapper.toDto(loginUser);
+    return ResponseEntity.ok(ResultResponse.of(GET_LOGIN_USER_SUCCESS, registerResponse));
+  }
+
 }
--- src/main/java/com/techeer/cokkiri/global/result/ResultCode.java
+++ src/main/java/com/techeer/cokkiri/global/result/ResultCode.java
@@ -15,6 +15,7 @@ public enum ResultCode {
   USER_USERNAME_NOT_DUPLICATED("U003", "회원 아이디 중복되지 않음"),
   USER_LOGIN_SUCCESS("U004", "회원 로그인 성공"),
   USER_LOGOUT_SUCCESS("U005", "회원 로그아웃 성공"),
+  GET_LOGIN_USER_SUCCESS("U006", "로그인 되어있는 회원 조회 성공"),
 
   // study
   STUDY_CREATE_SUCCESS("S001", "스터디 생성 성공"),
