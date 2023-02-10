98f1e97cbd74b82f9c269c6897c57ab83f08f9ed
kimich <ickim1218@gmail.com>
2023-02-11 00:37:59 +0900

    feat: test

--- src/main/java/com/techeer/cokkiri/test/test.java
+++ src/main/java/com/techeer/cokkiri/test/test.java
@@ -2,6 +2,8 @@ package com.techeer.cokkiri.test;
 
 public class test {
   public static void main(String[] args) {
+
     System.out.println("hello world");
+    System.out.println("Hello World");
   }
 }
