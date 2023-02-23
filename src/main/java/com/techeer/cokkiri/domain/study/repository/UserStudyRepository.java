b7dc2d525763ab0078e5e190ce5686430bf07657
JeongHunHui <deca3248@gmail.com>
2023-02-23 22:58:35 +0900

    fix: studyId로 해당 스터디의 유저들을 찾는 method 오류 수정

--- src/main/java/com/techeer/cokkiri/domain/study/repository/UserStudyRepository.java
+++ src/main/java/com/techeer/cokkiri/domain/study/repository/UserStudyRepository.java
@@ -4,7 +4,10 @@ import com.techeer.cokkiri.domain.user.entity.User;
 import com.techeer.cokkiri.domain.user.entity.UserStudy;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserStudyRepository extends JpaRepository<UserStudy, Long> {
-  List<User> findByStudyId(Long studyId);
+  @Query("SELECT u FROM User u JOIN u.studies us WHERE us.study.id = :studyId")
+  List<User> findByStudyId(@Param("studyId") Long studyId);
 }
